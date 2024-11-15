bbb04273061cae7fc40c9c03201173846f08e819
ChihaluPrg <saki.prg.20@gmail.com>
2024-11-15 11:01:20 +0900

    コードの移行

---
+++ OpenCV2.py
@@ -0,0 +1,52 @@
+import cv2
+import subprocess
+import time
+import os
+from ADB import capture_screen, check_device_connected, adb_path,  device_id
+
+# テンプレート画像を探す関数
+def find_template_position(template_path, threshold=0.8):
+    if not capture_screen():
+        print("キャプチャに失敗しました。")
+        return None
+    screen = cv2.imread("screen.png")
+    template = cv2.imread(template_path, 0)
+    if screen is None or template is None:
+        print("画像ファイルが見つかりません。パスを確認してください。")
+        return None
+    screen_gray = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+    result = cv2.matchTemplate(screen_gray, template, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+    if max_val >= threshold:
+        # 画像中央の座標を計算
+        template_height, template_width = template.shape[:2]
+        center_x = max_loc[0] + template_width // 2
+        center_y = max_loc[1] + template_height // 2
+        return (center_x, center_y)  # 中央の座標を返す
+    else:
+        return None
+
+
+# 実機上でタッチ操作を実行する関数
+def tap_on_device(x, y):
+    if not check_device_connected():
+        return False
+    # adbコマンドでタッチ操作を実行
+    subprocess.run([adb_path, "-s", device_id, "shell", f"input tap {x} {y}"])
+    return True
+
+# スクロール操作を実行する関数
+def scroll_on_device(start_x, start_y, end_x, end_y, duration=300):
+    """
+    start_x, start_y: スクロール開始の座標
+    end_x, end_y: スクロール終了の座標
+    duration: スクロールにかける時間（ミリ秒単位）
+    """
+    if not check_device_connected():
+        return False
+    # adbコマンドでドラッグ操作（スクロール）を実行
+    subprocess.run(
+        [adb_path, "-s", device_id, "shell", "input", "swipe", str(start_x), str(start_y), str(end_x), str(end_y),
+         str(duration)])
+    return True
