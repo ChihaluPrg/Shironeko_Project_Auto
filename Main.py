a5977bbbdb7ce54411dac1244fc6e5d32115d0da
ChihaluPrg <saki.prg.20@gmail.com>
2024-11-15 11:04:53 +0900

    起動処理を追加

--- Main.py
+++ Main.py
@@ -3,9 +3,10 @@ import subprocess
 import time
 import os
 from Launch import launch_game
-from ADB import set_tcpip_mode_without_device_check, connect_device, capture_screen, check_device_connected
-
-
+from ADB import set_tcpip_mode_without_device_check, connect_device
 
 if __name__ == "__main__":
+
+    # 白猫プロジェクトを起動
     launch_game()
+    time.sleep(2)
