6619d3fb4fabe7b5c78d96a0ef81e27193aff0a5
ChihaluPrg <saki.prg.20@gmail.com>
2024-11-16 22:42:06 +0900

    広告を閉じる画像を探す処理の修正

--- Advertisement.py
+++ Advertisement.py
@@ -1,7 +1,7 @@
 from OpenCV2 import find_template_position, tap_on_device
 import time
 
-# 広告の閉じるボタン（ad_close1～ad_close13）を探してタップする関数
+# 広告の閉じるボタン（ad_close1～ad_close16）を探してタップする関数
 def close_ad():
     """
     広告の閉じるボタン（ad_close1～ad_close13）のいずれかを見つけてタップします。
@@ -15,7 +15,7 @@ def close_ad():
         ad_found = False  # 1回の試行内で広告が見つかったかどうか
 
         # 広告閉じボタンを1回目または再試行で確認
-        for i in range(1, 16):
+        for i in range(1, 17): # (1, 17)でad_close1～ad_close16までが表示される
             ad_close_template_path = f"img/ad_close{i}.jpg"
             position = find_template_position(ad_close_template_path)
             if position:
