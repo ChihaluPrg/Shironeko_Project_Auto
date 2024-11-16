fd4328d98d0017cded362f78f74d32a9c327a34d
ChihaluPrg <saki.prg.20@gmail.com>
2024-11-17 07:45:09 +0900

    画像を探す回数を増やしました

--- Advertisement.py
+++ Advertisement.py
@@ -14,7 +14,7 @@ def close_ad():
         attempts += 1
         ad_found = False  # 1回の試行内で広告が見つかったかどうか
 
-        #広告閉じボタンを1回目または再試行で確認
+        # 広告閉じボタンを1回目または再試行で確認
         for i in range(1, 18):
             ad_close_template_path = f"img/ad_close{i}.jpg"
             position = find_template_position(ad_close_template_path)
