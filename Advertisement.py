4aff99b2af42de22d1384446e0222e4e62a56efa
ChihaluPrg <saki.prg.20@gmail.com>
2024-11-20 00:15:54 +0900

    閉じるまでの待機時間を追加

--- Advertisement.py
+++ Advertisement.py
@@ -1,18 +1,21 @@
 from OpenCV2 import find_template_position, tap_on_device
 import time
 
-# 広告の閉じるボタン（ad_close1～ad_close16）を探してタップする関数
+AD_CLOSE_IMG_PATH = "img/ad_close23.jpg"
+
 def close_ad():
     ad_closed = False
     attempts = 0  # 最大試行回数を追跡
+    threshold = 0.9  # 認識度の閾値を設定
     while attempts < 4 and not ad_closed:
         attempts += 1
         ad_found = False  # 1回の試行内で広告が見つかったかどうか
 
         # 広告閉じボタンを1回目または再試行で確認
-        for i in range(1, 23):
+        time.sleep(50)
+        for i in range(1, 27):
             ad_close_template_path = f"img/ad_close{i}.jpg"
-            position = find_template_position(ad_close_template_path)
+            position = find_template_position(ad_close_template_path, threshold=threshold)
             if position:
                 tap_on_device(position[0], position[1])
                 ad_found = True
@@ -31,3 +34,15 @@ def close_ad():
             # 最後の試行でも見つからなければ終了
             print(f"{attempts}個目で広告の閉じるボタンが見つかりませんでした。再試行はしません...")
             break  # 4回試行しても見つからなかった場合は終了
+
+"""
+ポインタ待ち
+
+def close_ad_img():
+    position = find_template_position(AD_CLOSE_IMG_PATH, threshold=0.9)
+    if position:
+        tap_on_device(position[0], position[1])
+    else:
+        print()
+
+"""
