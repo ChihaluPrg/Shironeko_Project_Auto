e323b2a4e63bb0efd5d63a7b1aa433bd24576fe0
ChihaluPrg <saki.prg.20@gmail.com>
2024-11-21 20:56:06 +0900

    待機時間などの修正

--- Advertisement.py
+++ Advertisement.py
@@ -1,7 +1,8 @@
 from OpenCV2 import find_template_position, tap_on_device
-import time
+from Countdown import countdown
 
 AD_CLOSE_IMG_PATH = "img/ad_close23.jpg"
+AD_CLOSE_UMA_PATH = "img/ad_close27.jpg"
 
 def close_ad():
     ad_closed = False
@@ -12,8 +13,7 @@ def close_ad():
         ad_found = False  # 1回の試行内で広告が見つかったかどうか
 
         # 広告閉じボタンを1回目または再試行で確認
-        time.sleep(50)
-        for i in range(1, 27):
+        for i in range(1, 28):
             ad_close_template_path = f"img/ad_close{i}.jpg"
             position = find_template_position(ad_close_template_path, threshold=threshold)
             if position:
@@ -25,24 +25,28 @@ def close_ad():
         if ad_found:
             # 見つかった場合、10秒後に再確認
             print(f"{attempts}個目の広告を閉じました。10秒後に再度広告の閉じるボタンを確認します...")
-            time.sleep(10)
+            countdown(10)
         elif attempts < 4:
             # 見つからなければ再試行
             print(f"{attempts}個目で広告の閉じるボタンが見つかりませんでした。再試行します...")
-            time.sleep(5)  # 再試行の前に少し待機
+            countdown(5) # 再試行の前に少し待機
         else:
             # 最後の試行でも見つからなければ終了
             print(f"{attempts}個目で広告の閉じるボタンが見つかりませんでした。再試行はしません...")
             break  # 4回試行しても見つからなかった場合は終了
 
-"""
-ポインタ待ち
+
 
 def close_ad_img():
     position = find_template_position(AD_CLOSE_IMG_PATH, threshold=0.9)
     if position:
-        tap_on_device(position[0], position[1])
+        tap_on_device(990, 170)
     else:
         print()
 
-"""
+def uma_ad_close():
+    position = find_template_position(AD_CLOSE_UMA_PATH, threshold=0.9)
+    if position:
+        tap_on_device(1030, 130)
+    else:
+        print()
